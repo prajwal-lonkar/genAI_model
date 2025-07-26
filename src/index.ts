355d8e94a6dca89c6f15e81d627af626149f58fd
Prajwal Lonkar <prajwallonkar007@gmail.com>
2025-07-26 16:19:08 +0530

    langchain implementation done

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,45 @@
-import { ChromaClient, EmbeddingFunction } from "chromadb";
-import { OpenAI } from "openai";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+const model = new ChatOpenAI({
+  modelName: "gpt-4o-mini",
+  maxTokens: 500,
 });
 
-const chromaClient = new ChromaClient({
-  host: "localhost",
-  port: 8000,
-});
+// const main = async () => {
+//   // For direct single output.
+//   // const response = await model.invoke("What is capital of Italy?")
+//   // console.log(response.content);
 
-class OpenAIEmbedding implements EmbeddingFunction {
-  async generate(texts: string[]): Promise<number[][]> {
-    const response = await openai.embeddings.create({
-      model: "text-embedding-3-small",
-      input: texts,
-    });
-    return response.data.map((embedding) => embedding.embedding);
-  }
-}
+//   // For output for multiple questions.
+//   // const response = await model.batch(["Hello", "What is capital of Italy?"]);
+//   // console.log(response);
 
-// Initialize collection
-const init = async () => {
-  const embedder = new OpenAIEmbedding();
-  const collection = await chromaClient.createCollection({
-    name: "my_collection",
-    embeddingFunction: embedder,
+//   // For stream like output
+//   //   const response = await model.stream("What is the capital of Italy?");
+//   //   for await (const chunk of response) {
+//   //     console.log(chunk.content);
+//   //   }
+// };
+// main();
+
+const run = async () => {
+  const systemTemplate = "Translate the following form English into {language}";
+
+  const promptTemplate = ChatPromptTemplate.fromMessages([
+    ["system", systemTemplate],
+    ["user", "{text"],
+  ]);
+
+  const promptValue = await promptTemplate.invoke({
+    language: "marathi",
+    text: "Hello everyone",
   });
-  return collection;
+  promptValue.toChatMessages();
+  const response = await model.invoke(promptValue);
+  console.log(response.content);
 };
-
-const addMessages = async (id: string, text: string) => {
-  const collection = await init();
-  await collection.add({
-    ids: [id],
-    documents: [text],
-  });
-  console.log(`Added text : ${text}`);
-};
-
-// Get similar messages
-const getSimilarMessages = async (text: string, limit: number) => {
-  const collection= await init();
-  const results = await collection.query({
-    queryTexts:[text],
-    nResults:limit
-  })
-  console.log(`Similar messages ${results.documents[0]}`);
-  return results.documents[0];
-};
-
-const run = async() => {
-  await addMessages('1' , 'Hello , how can i help you today?')
-  await addMessages('2' , 'Sure I can book your appointment')
-  await addMessages('3' , 'The weather is sunny and warm.')
-
-  await getSimilarMessages('Can you help me with a booking?',1)
-}
-run(); 
+run();
